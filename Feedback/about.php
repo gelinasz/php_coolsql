3fb97c0552d281a5d6633e769797c0511c51998d
C.W <64995170+CWGelinas@users.noreply.github.com>
2022-06-27 11:54:54 -0400

    initial committ feedback project

---
+++ Feedback/about.php
@@ -0,0 +1,13 @@
+<?php include 'inc/header.php'; ?>
+    
+<h2>About</h2>
+
+    <p class="text-center">Lorem ipsum 
+    dolor sit amet consectetur, 
+    adipisicing elit. Inventore impedit 
+    totam porro iure reiciendis autem 
+    possimus sapiente, optio, 
+    exercitationem ipsum assumenda mollitia, r
+    ecusandae expedita culpa ratione 
+    voluptatem esse quos quam?</p>
+<?php include 'inc/footer.php'; ?>
